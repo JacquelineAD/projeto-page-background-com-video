c95d8477edb76b218622d8c55b8930905374b51e
Jacqueline Andreia D <songziyansong1@gmail.com>
2023-11-19 17:07:34 -0300

    Alterações de medidas e Javascript adicionado
    Funçôes javascript para abrir e fechar a janela de login adicionadas. ajuste nas medidas do poster lateral

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,14 @@
+
+
 const container = document.querySelector('.container');
-const loginLink = document.querySelector();
+const botaoFechar = document.querySelector('.icone-fechar');
 
+document.getElementById('btnLogin').addEventListener('click', function() {
+    
+    container.classList.toggle('container-visivel');
+});
 
+document.getElementById('fecharJanela').addEventListener('click', function(){
+
+    container.classList.remove('container-visivel');
+});
